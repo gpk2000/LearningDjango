294ed3e898a725626415f2236a995c52e5a8df34
gpk2000 <codeforces.my@gmail.com>
2021-02-15 12:36:46 +0530

    completed chapter 4

--- MessageBoard/posts/tests.py
+++ MessageBoard/posts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="Test text")
+    
+    def test_text_content(self):
+        post=Post.objects.get(id=1)
+        expected_object_name=f"{post.text}"
+        self.assertEqual(expected_object_name, "Test text")
+
+class HomePageView(TestCase):
+    def setUp(self):
+        Post.objects.create(text='This is another test')
+    
+    def test_url_exist_at_correct_loc(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
